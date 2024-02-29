74ff2f952d64d3c713b76293fc56b1082a50eb86
ben glasser <ben@fantasticvoyage.com>
2024-02-29 11:34:47 -0800

    test movie 3

--- src/app/work-detail/reaktLights/page.jsx
+++ src/app/work-detail/reaktLights/page.jsx
@@ -20,22 +20,22 @@ const reaktLights = () => {
           Your browser does not support the video tag.
         </video>
       </div> */}
-
+{/* 
       <div className="relative w-full flex flex-col items-center">
         <video autoPlay loop muted playsinline className="w-full h-auto">
           <source src={REAKT.REAKT_HERO_VID} type="video/mp4"/>
           <source src={REAKT.REAKT_HERO_WEBM} type="video/webm" />
           Your browser does not support the video tag.
         </video>
-      </div>
+      </div> */}
 
-      {/* <div className=" relative w-full flex flex-col items-center">
+      <div className=" relative w-full flex flex-col items-center">
       <video className="w-full h-auto" autoPlay loop playsInline muted >
         <source src={"/EaselSizzle_v10.mp4"} type="video/mp4" />
         <source src={"/EaselSizzle_Vertical.webm"} type="video/webm" />
         Your browser does not support the video tag.
       </video>
-      </div> */}
+      </div>
 
       <ZigzagHeader
         title="REAKT LIGHTS"

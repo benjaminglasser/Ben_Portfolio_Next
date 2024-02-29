7ca9a38ac4ef3f7c84540f48280597533fda264e
ben glasser <ben@fantasticvoyage.com>
2024-02-29 13:14:55 -0800

    movie reakt test 7

--- src/app/common/VideoPlayerHero.jsx
+++ src/app/common/VideoPlayerHero.jsx
@@ -8,7 +8,7 @@ const VideoPlayerHero = ({}) => {
     <div className="relative w-full flex flex-col items-center">
       <video className="w-full h-auto" autoPlay loop playsInline muted >
         <source src={"/EaselSizzle_v10.mp4"} type="video/mp4" />
-        <source src={"/EaselSizzle_Vertical.webm"} type="video/webm" />
+        {/* <source src={"/EaselSizzle_Vertical.webm"} type="video/webm" /> */}
         Your browser does not support the video tag.
       </video>
 

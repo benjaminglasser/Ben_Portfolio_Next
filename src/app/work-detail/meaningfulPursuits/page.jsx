aa1d35aae2591c848c5e989c08f60f8c8f7b79aa
ben glasser <ben@fantasticvoyage.com>
2024-03-01 18:12:26 -0800

    plantasia test vid

--- src/app/work-detail/meaningfulPursuits/page.jsx
+++ src/app/work-detail/meaningfulPursuits/page.jsx
@@ -4,17 +4,17 @@ import ZigzagHeader from "../../common/zigzagHeader";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import FancyCarousel from "@/app/common/Carousel";
-import { MEAN } from "@/app/assets/images/meaningfulPursuits";
+// import { MEAN } from "@/app/assets/images/meaningfulPursuits";
 import VideoPlayerHero from '@/app/common/VideoPlayerHero.jsx'
 
 const MeaningfulPursuits = () => {
   return (
     <div className="w-full text-white">
-
+{/* 
       <VideoPlayerHero 
         className="md:h-[600px]"
         video={MEAN.MEAN_HERO_MAIN}
-      />
+      /> */}
 
       <ZigzagHeader
         title="MEANINGFUL PURSUITS"

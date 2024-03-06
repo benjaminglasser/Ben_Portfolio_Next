5dc7d21b3ff0ecac6433fb70b05415dffa75539d
ben glasser <ben@fantasticvoyage.com>
2024-03-05 18:17:35 -0800

    added nrf videos 2

--- src/app/common/ImageGrid.jsx
+++ src/app/common/ImageGrid.jsx
@@ -1,22 +1,24 @@
 function ImageGrid({ images }) {
   return (
     <div className="mx-auto my-8 md:my-12 md:w-3/5">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className={`flex ${
-            index % 2 === 0 ? "justify-start" : "justify-end"
-          } mb-4`}
-          //   style={{ marginTop: `${index * 0.03}rem` }} // Incremental offset
-        >
-          <div className="flex flex-col items-start">
-            <img src={image.url} alt={image.caption} className="max-w-lg" />{" "}
-            {/* Adjust max-w-xs for smaller images */}
-            <h2 className="mt-2">{image.title}</h2>
-            <p className="mt-2">{image.caption}</p>
+      <div className="flex flex-wrap justify-center gap-4 md:gap-8">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`flex flex-col ${
+              index % 2 === 0 ? "items-start" : "items-end"
+            }`}
+          >
+            <img
+              src={image.url}
+              alt={image.caption}
+              loading="lazy"
+              className="max-w-xs shadow-lg"
+            />
+            <p className="text-center mt-2">{image.caption}</p>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }

858f8b83d2ed4b17464555f5c1c2b2c04bc745e9
ben glasser <ben@fantasticvoyage.com>
2024-08-26 08:12:46 -0700

    updated adobe work experience

--- src/app/common/HomePageExtraInfo.jsx
+++ src/app/common/HomePageExtraInfo.jsx
@@ -6,21 +6,21 @@ const HomePageExtraInfo = () => {
       <div className="flex flex-col justify-between h-full relative">
         <p className="tag">
           <i className="text-sm text-white">
-            product designer, media artist + musician
+            designer, media artist + musician
           </i>
         </p>
         <div className="description absolute right-0 bottom-0">
-          <p className="text-end text-white">Born 1994</p>
+          <p className="text-end text-white">born 1994</p>
           <p className="text-end text-white">34.0766°N, -118.2593°W</p>
           <p className="text-end text-white">
-            lead product designer at{" "}
+            experience designer at{" "}
             <span className="ojuju text-white hover:text-black">
               <Link
-                href="https://easelapps.ai"
+                href="https://cdn.sanity.io/images/bl383u0v/production/b8bf3938336ab457e1045e90fe737ce0c59f1cde-1500x1000.jpg?rect=0,106,1500,788&w=1200&h=630&q=70&fit=crop&auto=format"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Easel AI
+                adobe
               </Link>
             </span>
           </p>
